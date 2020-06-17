31f75fdfcf51df5868e8e7b218c848671640cc40
Yang Hu <hu.yang@nyu.edu>
2020-06-16 21:56:06 -0400

    update PrepareShiny

--- R/Seurat3_functions.R
+++ R/Seurat3_functions.R
@@ -2493,62 +2493,67 @@ PCAPlot.1 <- function(object,dims = c(1, 2),cells = NULL,cols = NULL, pt.size = 
 }
 
 
-
+#' prepare exp and tsne file
+#' #' @param split.by split objecty by. Colname of meta.data. Set to FALSE if want to use all 
 PrepareShiny <- function(object, samples, Rshiny_path, split.by = "orig.ident",reduction = "tsne",
                          verbose = F,scale =NULL, assay = NULL){
-        if(missing(object) | class(object) != "Seurat") stop("samples is not provided")
-        if(missing(samples)) stop("samples is not provided")
-        if(missing(Rshiny_path)) stop("Rshiny_path is not provided")
-        assay = DefaultAssay(object) %||% assay
-        Idents(object) <-  split.by
-        avaible_samples <- samples %in% c("All_samples",object@meta.data[,split.by])
-        if (!all(avaible_samples))
-                stop(paste(paste(samples[!avaible_samples],collapse = " "),
-                           "are not exist in the data."))
-        if("All_samples" %in% samples) {
-                single_object <- object
-        } else single_object <- subset(object, idents = samples)
-        max_exp <- list()
+    if(missing(object) | class(object) != "Seurat") stop("samples is not provided")
+    if(missing(samples)) stop("samples is not provided")
+    if(missing(Rshiny_path)) stop("Rshiny_path is not provided")
+    assay = DefaultAssay(object) %||% assay
+    Idents(object) <-  split.by
+    avaible_samples <- samples %in% c("All_samples",object@meta.data[,split.by])
+    if (!all(avaible_samples))
+        stop(paste(paste(samples[!avaible_samples],collapse = " "),
+                   "are not exist in the data."))
+    max_exp <- list()
+    if("All_samples" %in% samples) {
+        single_object <- object
+    } else single_object <- subset(object, idents = samples)
+    if(!is.null(scale)){
+        max_exp = qlcMatrix::rowMax(single_object[[assay]]@data) %>% as.vector()
+        max_exp = max_exp/log(2)
+        names(max_exp) = rownames(single_object)
+    }
+    
+    exp <- list()
+    tsne <- list()
+    for (i in seq_along(samples)){
+        sample <- samples[i]
+        if(sample == "All_samples") {
+            single_object <- object
+        } else single_object <- subset(object, idents = sample)
+        #============== exp csv===============
+        data <- GetAssayData(single_object)
+        data <- as(data, "sparseMatrix")
+        data = data/log(2)
+        #bad <- rowMax(data) == 0
+        #data = data[!bad,]
         if(!is.null(scale)){
-                max_exp = qlcMatrix::rowMax(single_object[[assay]]@data) %>% as.vector()
-                max_exp = max_exp/log(2)
-                names(max_exp) = rownames(single_object)
+            range <- rowMax(data) - rowMin(data) # range <- apply(data,1,max) - apply(data,1,min)
+            data = sweep(data, 1, range,"/")*scale
         }
         
-        exp <- list()
-        tsne <- list()
-        for (i in seq_along(samples)){
-                sample <- samples[i]
-                if(sample == "All_samples") {
-                        single_object <- object
-                } else single_object <- subset(object, idents = sample)
-                #============== exp csv===============
-                data <- GetAssayData(single_object)
-                data <- as(data, "sparseMatrix")
-                data = data/log(2)
-                #bad <- rowMax(data) == 0
-                #data = data[!bad,]
-                if(!is.null(scale)){
-                        range <- rowMax(data) - rowMin(data) # range <- apply(data,1,max) - apply(data,1,min)
-                        data = sweep(data, 1, range,"/")*scale
-                }
-                
-                if(verbose) {
-                        print(sample)
-                        print(format(object.size(data),units="MB"))
-                }
-                exp[[i]] = data
-                #============== tsne csv===============
-                tsne[[i]] = Embeddings(single_object, reduction = reduction)
-                
-                svMisc::progress(i/length(samples)*100)
+        if(verbose) {
+            print(sample)
+            print(format(object.size(data),units="MB"))
         }
-        names(exp) = samples
-        names(tsne) = samples
-        shiny_data_path <- paste0(Rshiny_path, "data/")
-        if(!dir.exists(shiny_data_path)) dir.create(shiny_data_path, recursive = T)
+        exp[[i]] = data
+        #============== tsne csv===============
+        tsne[[i]] = Embeddings(single_object, reduction = reduction)
+        
+        svMisc::progress(i/length(samples)*100)
+    }
+    names(exp) = samples
+    names(tsne) = samples
+    shiny_data_path <- paste0(Rshiny_path, "data/")
+    if(!dir.exists(shiny_data_path)) dir.create(shiny_data_path, recursive = T)
+    if(!is.null(scale)){
         save(exp,tsne,max_exp, file = paste0(shiny_data_path,basename(Rshiny_path),".Rda"))
+    } else save(exp,tsne, file = paste0(shiny_data_path,basename(Rshiny_path),".Rda"))
+    
 }
+
 
 
 TitleCenter <- function(size = 15, hjust = 0.5){
