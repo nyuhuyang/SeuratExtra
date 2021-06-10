c75a2a9f97c051f86d22aa685bdd69fd9bdc4a70
Yang Hu <hu.yang@nyu.edu>
2021-06-10 13:15:12 -0400

    update PrepareGSEA

--- R/Seurat3_functions.R
+++ R/Seurat3_functions.R
@@ -9,7 +9,7 @@ library(Matrix)
 library(qlcMatrix)
 library(cowplot)
 library(cowplot)
-
+library(made4)
 # Set a default value if an object is nullf
 #
 # @param lhs An object to set if it's null
@@ -2314,6 +2314,127 @@ Human2Mouse <- function(x){
     return(humanx)
 }
 
+# add spearman correlation
+heatplot.1 <- function (dataset, dend = c("both", "row", "column", "none"), 
+          cols.default = TRUE, lowcol = "green", highcol = "red", cor.method = "spearman",
+          scale = "none", classvec = NULL, classvecCol = NULL, classvec2 = NULL, 
+          distfun = NULL, returnSampleTree = FALSE, method = "ave", 
+          dualScale = TRUE, zlim = c(-3, 3), scaleKey = TRUE, ...) 
+{
+        library(gplots)
+        data <- array2ade4(dataset)
+        data <- as.matrix(data)
+        if (dualScale) {
+                print(paste("Data (original) range: ", round(range(data), 
+                                                             2)[1], round(range(data), 2)[2]), sep = "")
+                data <- t(scale(t(data)))
+                print(paste("Data (scale) range: ", round(range(data), 
+                                                          2)[1], round(range(data), 2)[2]), sep = "")
+                data <- pmin(pmax(data, zlim[1]), zlim[2])
+                print(paste("Data scaled to range: ", round(range(data), 
+                                                            2)[1], round(range(data), 2)[2]), sep = "")
+        }
+        distEisen <- function(x, use = "pairwise.complete.obs") {
+                co.x <- cor(x, use = use, method = cor.method)
+                dist.co.x <- 1 - co.x
+                return(as.dist(dist.co.x))
+        }
+        cols <- function(low = lowcol, high = highcol, ncolors = 123) {
+                low <- col2rgb(low)/255
+                if (is.character(high)) 
+                        high <- col2rgb(high)/255
+                col <- rgb(seq(low[1], high[1], len = ncolors), seq(low[2], 
+                                                                    high[2], len = ncolors), seq(low[3], high[3], len = ncolors))
+                return(col)
+        }
+        cols.gentleman <- function() {
+                library(RColorBrewer)
+                hmcol <- colorRampPalette(brewer.pal(10, "RdBu"))(256)
+                return(rev(hmcol))
+        }
+        if (cols.default) 
+                plotcols = cols.gentleman()
+        else plotcols = cols()
+        if (is.null(distfun)) 
+                distf = distEisen
+        else distf = function(x) dist(t(x), method = distfun)
+        Colv <- FALSE
+        Rowv <- FALSE
+        dend <- match.arg(dend)
+        dend = tolower(dend)
+        if (dend %in% c("row", "r")) {
+                print(dend)
+                Rowv = as.dendrogram(hclust(distf(t(data)), method = method))
+        }
+        if (dend %in% c("column", "col", "c")) {
+                print(dend)
+                Colv = as.dendrogram(hclust(distf(data), method = method))
+                dend = "column"
+        }
+        if (dend %in% c("both", "TRUE")) {
+                Colv = as.dendrogram(hclust(distf(data), method = method))
+                Rowv = as.dendrogram(hclust(distf(t(data)), method = method))
+                dend = "both"
+        }
+        RSideColors = CSideColors = NULL
+        if (any(!is.null(classvec), !is.null(classvec2))) {
+                proc.classvec <- function(classvec) {
+                        classvec = as.factor(classvec)
+                        if (is.null(classvecCol)) 
+                                classvecCol = getcol(length(levels(classvec)))
+                        SideCols = factor(classvec, labels = classvecCol)
+                        print(cbind(Class = levels(classvec), Color = levels(SideCols)))
+                        SideCols = as.character(SideCols)
+                        nSC = length(SideCols)
+                        return(list(nSC, SideCols))
+                }
+                if (!is.null(classvec)) {
+                        out = proc.classvec(classvec)
+                        nSC = out[[1]]
+                        SideCols = out[[2]]
+                        if (!nSC %in% dim(data)) 
+                                print("Error: classvec length not equal to nrow or ncol in data")
+                        if (nSC == nrow(data)) 
+                                RSideColors = SideCols
+                        if (nSC == ncol(data)) 
+                                CSideColors = SideCols
+                }
+                if (!is.null(classvec2)) {
+                        out = proc.classvec(classvec2)
+                        nSC = out[[1]]
+                        SideCols = out[[2]]
+                        if (!nSC %in% dim(data)) 
+                                print("Error: classvec2 length not equal to nrow or ncol in data")
+                        if (nSC == nrow(data)) 
+                                RSideColors = SideCols
+                        if (nSC == ncol(data)) 
+                                CSideColors = SideCols
+                }
+        }
+        if (all(is.null(RSideColors), is.null(CSideColors))) 
+                heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, 
+                          scale = scale, trace = "none", density.info = "none", 
+                          zlim = zlim, dendrogram = dend, ...)
+        if (all(!is.null(RSideColors), is.null(CSideColors))) 
+                heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, 
+                          scale = scale, trace = "none", density.info = "none", 
+                          RowSideColors = RSideColors, dendrogram = dend, 
+                          zlim = zlim, ...)
+        if (all(is.null(RSideColors), !is.null(CSideColors))) 
+                heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, 
+                          scale = scale, trace = "none", density.info = "none", 
+                          ColSideColors = CSideColors, dendrogram = dend, 
+                          zlim = zlim, ...)
+        if (all(!is.null(RSideColors), !is.null(CSideColors))) 
+                heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, 
+                          scale = scale, trace = "none", density.info = "none", 
+                          RowSideColors = RSideColors, ColSideColors = CSideColors, 
+                          zlim = zlim, dendrogram = dend, ...)
+        if (returnSampleTree) 
+                return(Colv)
+}
+
+
 #' Convert list to data frame
 #'
 #' This function will convert a list to a data frame, even if they are unequal length
@@ -2666,41 +2787,43 @@ paramSweep_v4 <- function (seu, PCs = 1:10, sct = FALSE)
 #' @param do.return TRUE/FALSE
 #' @param continuous.label NULL/continuous label #http://software.broadinstitute.org/gsea/doc/GSEAUserGuideFrame.html?_Phenotype_Labels
 #' @example PrepareGSEA(object, k = 50, continuous.label = major_cells)
-PrepareGSEA <- function(object, k = 1, do.return = FALSE, continuous.label = NULL){
+PrepareGSEA <- function(object, k = 1, do.return = FALSE, continuous.label = NULL,...){
     
     set.seed(201)
-    object@scale.data = NULL
-    ident = FindIdentLabel(object)[1]
+    ident= FindIdentLabel(object)[1]
     
     if(k > 1){
-        split_object <- SplitSeurat(object, split.by = ident)
-        #Split object@ident to k group =====
-        for(i in 1:length(split_object)){
-            meta_index <- caret::createFolds(split_object[[i]]@meta.data[,ident],
+        #Split object meta.data into 2 group =====
+        split_meta.data <- split(object@meta.data, f = object@meta.data[,ident])
+        #Split each meta.data into k group =====
+        for(i in 1:length(split_meta.data)){
+            meta_index <- caret::createFolds(split_meta.data[[i]][,ident],
                                          k = k, list = TRUE, 
                                          returnTrain = FALSE)
             for(n in 1:length(meta_index)){
-                split_object[[i]]@meta.data[meta_index[[n]],"GSEA"] = 
-                    paste(split_object[[i]]@meta.data[meta_index[[n]],ident],
+                split_meta.data[[i]][meta_index[[n]],"GSEA"] = 
+                    paste(split_meta.data[[i]][meta_index[[n]],ident],
                           n, sep = "_")
                 }
         }
-        new_object <-  Reduce(MergeSeurat,split_object) %>%
-            SetAllIdent(id = "GSEA")
-        if(!is.null(continuous.label))
-            new_object@ident <- factor(x = new_object@ident,
-                                    levels = paste0(rep(continuous.label,each = k),
-                                                    "_",rep(1:k)))
-        } else new_object <- object
-    
-        if(k == 1 & !is.null(continuous.label)) {
-            new_object@ident <- factor(x = new_object@ident,
-                                       levels = continuous.label)
-        }
+    }
+    meta.data = bind_rows(split_meta.data)
+    object@meta.data = meta.data[rownames(object@meta.data),]
+
+    if(!is.null(continuous.label)) {
+        Idents(object) %<>% factor(levels = paste0(rep(continuous.label,each = k),
+                                                   "_",rep(1:k)))
+    }
+        
+
+    if(k == 1 & !is.null(continuous.label)) {
+        Idents(object) %<>% factor(levels = continuous.label)
+    }
 
                 
     print("#====Calculate Average Expression======")
-    GSEA_expr <- AverageExpression(new_object)
+    GSEA_expr <- AverageExpression(object, assays = DefaultAssay(object))
+    GSEA_expr = GSEA_expr[[1]]
     GSEA_name <- data.frame("NAME" = rownames(GSEA_expr),
                             "DESCRIPTION" = rep(NA,nrow(GSEA_expr)),
                             stringsAsFactors = F)
@@ -2738,7 +2861,7 @@ PrepareGSEA <- function(object, k = 1, do.return = FALSE, continuous.label = NUL
         GSEA_expr <- merge(genesV2,GSEA_expr,by = "gene")
         GSEA_expr = GSEA_expr[,-1]
     }
-    file.name = paste(unique(object@ident), collapse = "_")
+    file.name = paste(unique(Idents(object)), collapse = "_")
     if(!is.null(continuous.label)) file.name <- paste(continuous.label,collapse = "_")
     if(is.null(save.path)) save.path <- paste0("output/",gsub("-","",Sys.Date()))
     if(!dir.exists(save.path)) dir.create(save.path, recursive = T)
